a5de8f27c9a9aeab392fb6250311b3b5da446d78
Thomas <tchaidez@iastate.edu>
2020-12-15 04:12:33 -0600

    Updated Text

---
+++ PlayerData.js
@@ -0,0 +1,234 @@
+//= [0.0, 0.0, 0.0, 0.0, 0.0];
+/*
+
+1 Points 
+2 rebounds 
+3 assists 
+4 steals 
+5 blocks
+
+
+
+
+*/
+
+//Current NBA Starters as of 12-15-2020
+//Milwaukee Bucks
+var Jrue_Holiday = [15.9, 3.9, 6.4, 1.5, 0.5];
+var Donte_DiVincenzo = [8.0, 4.1, 2.0, 1.1, 0.3];
+var Khris_Middleton = [16.3, 4.6, 3.4, 1.2, 0.2];
+var Giannis_Antetokounmpo = [20.1, 8.9, 4.3, 1.2, 1.3];
+var Brook_Lopez = [16.8, 6.4, 1.5, 0.6, 1.8];
+
+//Toronto Raptors
+var Kyle_Lowry
+var Fred_VanVleet
+var OG_Anunoby
+var Pascal_Siakam
+var Aron_Baynes
+
+//Los Angeles Lakers
+var LeBron_James
+var Dennis_Schroder
+var Kentavious_Caldwell_Pope
+var Anthony_Davis
+var Marc_Gasol
+
+//LA Clippers
+var Patrick_Beverley
+var Paul_George
+var Kawhi_Leonard
+var Marcus_Morris_Sr
+var Serge_Ibaka
+
+//Boston Celtics
+var Kemba_Walker
+var Marcus_Smart
+var Jaylen_Brown
+var Jayson_Tatum
+var Daniel_Theis
+
+//Denver Nuggets
+var Jamal_Murray
+var Gary_Harris
+var Michael_Porter_Jr
+var Paul_Millsap
+var Nikola_Jokic
+
+//Indiana Pacers
+var Malcolm_Brogdon
+var Victor_Oladipo
+var TJ_Warren
+var Domantas_Sabonis
+var Myles_Turner
+
+//Houston Rockets
+var John_Wall
+var James_Harden
+var Eric_Gordon
+var PJ_Tucker
+var Christian_Wood
+
+//Oklahoma City Thunder
+var Shai_Gilgeous_Alexander
+var Hamidou_Diallo
+var Luguentz_Dort
+var Darius_Bazley
+var Al_Horford
+
+//Utah Jazz
+var Mike_Conley
+var Donovan_Mitchell
+var Bojan_Bogdanovic
+var Royce_O_Neale
+var Rudy_Gobert
+
+//Miami Heat
+var Goran_Dragic
+var Duncan_Robinson
+var Jimmy_Butler
+var Maurice_Harkless
+var Bam_Adebayo
+
+//Philadelphia 76ers
+var Ben_Simmons
+var Danny_Green
+var Matisse_Thybulle
+var Tobias_Harris
+var Joel_Embiid
+
+//Dallas Mavericks
+var Luka_Doncic
+var Tim_Hardaway_Jr
+var Josh_Richardson
+var Maxi_Kleber
+var Kristaps_Porzingis
+
+//Brooklyn Nets
+var Kyrie_Irving
+var Caris_LeVert
+var Joe_Harris
+var Kevin_Durant
+var Jarrett_Allen
+
+//Portland Trail Blazers
+var Damian_Lillard
+var CJ_McCollum
+var Derrick_Jones_Jr
+var Robert_Covington
+var Jusuf_Nurkic
+
+//Memphis Grizzlies
+var Ja_Morant
+var Dillon_Brooks
+var Kyle_Anderson
+var Jaren_Jackson_Jr
+var Jonas_Valanciunas
+
+//Phoenix Suns
+var Chris_Paul
+var Devin_Booker
+var Mikal_Bridges
+var Jae_Crowder 
+var Deandre_Ayton
+
+//Orlando Magic
+var Markelle_Fultz
+var Evan_Fournier
+var James_Ennis_III
+var Aaron_Gordon
+var Nikola_Vucevic
+
+//San Antonio Spurs
+var Dejounte_Murray
+var Derrick_White
+var DeMar_DeRozan
+var LaMarcus_Aldridge
+var Jakob_Poeltl
+
+//Sacramento Kings
+var DeAaron_Fox
+var Buddy_Hield
+var Harrison_Barnes
+var Marvin_Bagley_III
+var Richaun_Holmes
+
+//New Orleans Pelicans
+var Lonzo_Ball
+var Eric_Bledsoe
+var Brandon_Ingram
+var Zion_Williamson
+var Steven_Adams
+
+//Charlotte Hornets
+var Devonte_Graham
+var Terry_Rozier
+var Gordon_Hayward
+var PJ_Washington
+var Cody_Zeller
+
+//Washington Wizards
+var Russell_Westbrook
+var Bradley_Beal
+var Davis_Bertans
+var Rui_Hachimura
+var Thomas_Bryant
+
+//Chicago Bulls
+var Coby_White
+var Zach_LaVine
+var Otto_Porter_Jr
+var Lauri_Markkanen
+var Wendell_Carter_Jr
+
+//New York Knicks
+var Elfrid_Payton
+var Alec_Burks
+var RJ_Barrett
+var Julius_Randle
+var Nerlens_Noel
+
+//Detroit Pistons
+var Killian_Hayes
+var Delon_Wright
+var Jerami_Grant
+var Blake_Griffin
+var Mason_Plumlee
+
+//Atlanta Hawks
+var Trae_Young
+var Bogdan_Bogdanovic
+var DeAndre_Hunter
+var John_Collins
+var Clint_Capela
+
+//Minnesota Timberwolves
+var Ricky_Rubio
+var DAngelo_Russell
+var Anthony_Edwards
+var Juan_Hernangomez
+var KarlAnthony_Towns
+
+//Cleveland Cavaliers
+var Darius_Garland
+var Collin_Sexton
+var Isaac_Okoro
+var Kevin_Love
+var Andre_Drummond
+
+//Golden State Warriors
+var Stephen_Curry
+var Andrew_Wiggins
+var Kelly_Oubre_Jr
+var Draymond_Green
+var James_Wiseman
+
+function compare() {
+    
+    if(Jrue_Holiday[0] > Giannis_Antetokounmpo[0]){
+
+        
+    }
+
+    return Jrue_Holiday[0] 
+}
